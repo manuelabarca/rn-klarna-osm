6753e377ff54b9e8d6398f8963f8b0a505c0ccbe
Manuel Labarca <manuel.labarca@iskaypet.com>
2024-11-05 17:08:23 +0100

    downgrande sdk

--- android/src/main/java/com/rnklarnaosm/KlarnaOnsiteMessagingLayout.kt
+++ android/src/main/java/com/rnklarnaosm/KlarnaOnsiteMessagingLayout.kt
@@ -13,7 +13,7 @@ class KlarnaOnsiteMessagingLayout(
   private val reactContext: ThemedReactContext,
   private val eventEmitter: KlarnaOnsiteMessagingEventEmitter
 ): LinearLayout(reactContext) {
-  private val osmView: KlarnaOSMView
+  val osmView: KlarnaOSMView
 
   init {
     inflate(context, R.layout.klarna_onsite_messaging_layout, this)
@@ -22,30 +22,10 @@ class KlarnaOnsiteMessagingLayout(
     osmView.hostActivity = reactContext.currentActivity
   }
 
-  var clientId: String = ""
-  var placementKey: String = ""
-  var locale: String = ""
-  var region: Int = 0
-  var environment: Int = 0
-  var purchaseAmount: Int? = null
 
   fun setupOSMView() {
-    osmView.clientId = clientId
-    osmView.placementKey = placementKey
-    osmView.locale = locale
-    osmView.environment = KlarnaEnvironment::class.fromRawValue(environment)
-    osmView.region = KlarnaRegion::class.fromRawValue(region)
-    osmView.purchaseAmount = purchaseAmount?.toLong()
+
     osmView.hostActivity = reactContext.currentActivity
-
-      Log.d(TAG, "clientId: $clientId")
-    Log.d(TAG, "placementKey: $placementKey")
-    Log.d(TAG, "locale: $locale")
-    Log.d(TAG, "environment: ${KlarnaEnvironment::class.fromRawValue(environment)}")
-    Log.d(TAG, "region: ${KlarnaRegion::class.fromRawValue(region)}")
-    Log.d(TAG, "purchaseAmount: ${purchaseAmount?.toLong()}")
-    Log.d(TAG, "hostActivity: ${reactContext.currentActivity}")
-
     osmView.render(RenderResult {
       if(it != null) {
         eventEmitter.onOSMError(it, id)
--- android/src/main/java/com/rnklarnaosm/RnKlarnaOsmViewManager.kt
+++ android/src/main/java/com/rnklarnaosm/RnKlarnaOsmViewManager.kt
@@ -5,6 +5,8 @@ import android.view.View
 import com.facebook.react.uimanager.ViewGroupManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
+import com.klarna.mobile.sdk.api.KlarnaEnvironment
+import com.klarna.mobile.sdk.api.KlarnaRegion
 
 class RnKlarnaOsmViewManager : ViewGroupManager<KlarnaOnsiteMessagingLayout>() {
   override fun getName() = "RnKlarnaOsmView"
@@ -13,46 +15,47 @@ class RnKlarnaOsmViewManager : ViewGroupManager<KlarnaOnsiteMessagingLayout>() {
   @ReactProp(name = "clientId")
   fun setClientId(view: KlarnaOnsiteMessagingLayout, clientId: String) {
     with(view) {
-      this.clientId = clientId
+      this.osmView.clientId = clientId
     }
   }
 
     @ReactProp(name = "placementKey")
   fun setPlacementKey(view: KlarnaOnsiteMessagingLayout, placementKey: String) {
     with(view) {
-      this.placementKey = placementKey
+      this.osmView.placementKey = placementKey
     }
   }
 
   @ReactProp(name = "locale")
   fun setLocale(view: KlarnaOnsiteMessagingLayout, locale: String) {
     with(view) {
-      this.locale = locale
+      this.osmView.locale = locale
     }
   }
 
   @ReactProp(name = "environment")
   fun setEnvironment(view: KlarnaOnsiteMessagingLayout, environment: Int) {
     with(view) {
-      this.environment = environment
+      this.osmView.environment = KlarnaEnvironment::class.fromRawValue(environment)
     }
   }
 
   @ReactProp(name = "region")
   fun setRegion(view: KlarnaOnsiteMessagingLayout, region: Int) {
     with(view) {
-      this.region = region
+      this.osmView.region = KlarnaRegion::class.fromRawValue(region)
     }
   }
 
   @ReactProp(name = "purchaseAmount")
   fun setPurchaseAmount(view: KlarnaOnsiteMessagingLayout, purchaseAmount: Int?) {
     with(view) {
-      this.purchaseAmount = purchaseAmount
+      this.osmView.purchaseAmount = purchaseAmount?.toLong()
     }
   }
 
   override fun onAfterUpdateTransaction(view: KlarnaOnsiteMessagingLayout) {
+    super.onAfterUpdateTransaction(view)
     view.setupOSMView()
   }
 
